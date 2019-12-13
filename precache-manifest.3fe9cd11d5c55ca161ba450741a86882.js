994e8b240b407e3c59d28460cb5f59a112780c79
antonio <antonio@pykiss.com>
2019-12-13 10:10:36 +0100

    Updates

---
+++ precache-manifest.3fe9cd11d5c55ca161ba450741a86882.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6db5ae24df5cf737d076539c62a146e",
+    "url": "/ast/index.html"
+  },
+  {
+    "revision": "e06fe408bdbb48de7f8e",
+    "url": "/ast/static/css/main.3573e6ad.chunk.css"
+  },
+  {
+    "revision": "2a4d99ca0d619cad3e06",
+    "url": "/ast/static/js/2.1a45b1c1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ast/static/js/2.1a45b1c1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e06fe408bdbb48de7f8e",
+    "url": "/ast/static/js/main.55470340.chunk.js"
+  },
+  {
+    "revision": "8ae23bc71dd0c34d91b9",
+    "url": "/ast/static/js/runtime-main.de8886ba.js"
+  }
+]);
